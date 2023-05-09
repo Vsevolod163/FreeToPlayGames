e845aa039bac64ffcfd636ca165c063900176142
Vsevolod <vsevolod.lashin@gmail.com>
2023-05-10 02:16:13 +0300

    GamesCollectionViewCell added

---
+++ NetworkApp/View/GameCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  GameCollectionViewCell.swift
+//  NetworkApp
+//
+//  Created by Vsevolod Lashin on 10.05.2023.
+//
+
+import UIKit
+
+final class GameCollectionViewCell: UICollectionViewCell {
+    
+}
