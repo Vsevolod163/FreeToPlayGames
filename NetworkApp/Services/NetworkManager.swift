2df4f1ab6baac21cc95c6f38ea74ca3731b1c539
Vsevolod <vsevolod.lashin@gmail.com>
2023-05-10 02:15:00 +0300

    networkManager added

--- NetworkApp/Services/NetworkManager.swift
+++ NetworkApp/Services/NetworkManager.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+final class NetworkManager {
+    static var shared = NetworkManager()
+    
+    private let gamesURL = URL(string: "https://www.freetogame.com/api/games")!
+    
+    private init() {}
+    
+    func fetchGames() {
+        URLSession.shared.dataTask(with: gamesURL) { data, _, error in
+            guard let data else {
+                print(error?.localizedDescription ?? "No error description")
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                let games = try decoder.decode([Game].self, from: data)
+                print(games)
+            } catch {
+                print(error.localizedDescription)
+            }
+        }.resume()
+    }
+}
+
--- NetworkApp/ViewControllers/GameInfoViewController.swift
+++ NetworkApp/ViewControllers/GameInfoViewController.swift
@@ -9,31 +9,17 @@ import UIKit
 
 final class GameInfoViewController: UIViewController {
 
-    private let gamesURL = URL(string: "https://www.freetogame.com/api/games")!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        fetchGames()
+
+        
     }
 
 }
 
 // MARK: - Networking
 extension GameInfoViewController {
-    private func fetchGames() {
-        URLSession.shared.dataTask(with: gamesURL) { data, _, error in
-            guard let data else {
-                print(error?.localizedDescription ?? "No error description")
-                return
-            }
-            
-            do {
-                let decoder = JSONDecoder()
-                let games = try decoder.decode([Game].self, from: data)
-                print(games)
-            } catch {
-                print(error.localizedDescription)
-            }
-        }.resume()
-    }
+    
 }
