d7081d754143cd0863dd63d391778ca1df81c699
Vsevolod <vsevolod.lashin@gmail.com>
2023-05-10 04:30:17 +0300

    struct Requirements and struct Screenshots added

--- NetworkApp/Models/Game.swift
+++ NetworkApp/Models/Game.swift
@@ -8,13 +8,29 @@
 import Foundation
 
 struct Game: Decodable {
+    let id: Int
     let title: String
     let thumbnail: URL
     let short_description: String
+    let description: String?
     let game_url: String
     let genre: String
     let platform: String
     let publisher: String
     let developer: String
     let release_date: String
+    let minimum_system_requirements: Requirements?
+    let screenshots: [Screenshot]?
+}
+
+struct Requirements: Decodable {
+    let os: String
+    let processor: String
+    let memory: String
+    let graphics: String
+    let storage: String
+}
+
+struct Screenshot: Decodable {
+    let image: URL
 }
