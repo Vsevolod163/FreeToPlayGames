b5be9eefba015657d748e4e4070640d67bec53f9
Vsevolod <vsevolod.lashin@gmail.com>
2023-05-10 03:50:52 +0300

    thumbnail type changed to URL

--- NetworkApp/Models/Game.swift
+++ NetworkApp/Models/Game.swift
@@ -5,9 +5,11 @@
 //  Created by Vsevolod Lashin on 06.05.2023.
 //
 
+import Foundation
+
 struct Game: Decodable {
     let title: String
-    let thumbnail: String
+    let thumbnail: URL
     let short_description: String
     let game_url: String
     let genre: String
