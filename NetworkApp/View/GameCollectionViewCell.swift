a7a7ce5820eb67d92e304d24730d2ed2527bda80
Vsevolod <vsevolod.lashin@gmail.com>
2023-05-10 03:49:16 +0300

    configure func added

--- NetworkApp/View/GameCollectionViewCell.swift
+++ NetworkApp/View/GameCollectionViewCell.swift
@@ -9,4 +9,18 @@ import UIKit
 
 final class GameCollectionViewCell: UICollectionViewCell {
     
+    @IBOutlet var gameImageView: UIImageView!
+    
+    private let networkManager = NetworkManager.shared
+    
+    func configure(with game: Game) {
+        networkManager.fetchImage(from: game.thumbnail) { [weak self] result in
+            switch result {
+            case .success(let imageData):
+                self?.gameImageView.image = UIImage(data: imageData)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
 }
