57acae95279f6ec936d110f286d59a4f80834694
Vsevolod <vsevolod.lashin@gmail.com>
2023-05-10 04:34:28 +0300

    gameId property and func fetchGame added

--- NetworkApp/ViewControllers/GameInfoViewController.swift
+++ NetworkApp/ViewControllers/GameInfoViewController.swift
@@ -9,15 +9,32 @@ import UIKit
 
 final class GameInfoViewController: UIViewController {
 
-    var game: Game!
+    var gameId: Int!
+    
+    private var currentGame: Game! = nil
+    private let networkManager = NetworkManager.shared
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
     }
 }
 
 // MARK: - Networking
 extension GameInfoViewController {
-    
+    func fetchGame() {
+        let gameURL = URL(string: "https://www.freetogame.com/api/game?id=\(gameId ?? 0)")!
+       
+        networkManager.fetch(Game.self, from: gameURL) { [weak self] result in
+            switch result {
+            case .success(let game):
+                self?.currentGame = game
+                print(game)
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
 }
+
+
