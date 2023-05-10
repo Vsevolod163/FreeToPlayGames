37448a3eb7fa38c64d23a0bc4e6a8857aa6cb81e
Vsevolod <vsevolod.lashin@gmail.com>
2023-05-10 03:49:43 +0300

    fetchImage func added

--- NetworkApp/Services/NetworkManager.swift
+++ NetworkApp/Services/NetworkManager.swift
@@ -7,6 +7,12 @@
 
 import Foundation
 
+enum NetworkError: Error {
+    case invalidURL
+    case noData
+    case decodingError
+}
+
 final class NetworkManager {
     static var shared = NetworkManager()
     
@@ -14,7 +20,19 @@ final class NetworkManager {
     
     private init() {}
     
-    func fetchGames() {
+    func fetchImage(from url: URL, completion: @escaping (Result<Data, NetworkError>) -> Void) {
+        DispatchQueue.global().async {
+            guard let imageData = try? Data(contentsOf: url) else {
+                completion(.failure(.noData))
+                return
+            }
+            Dispatch.DispatchQueue.main.async {
+                completion(.success(imageData))
+            }
+        }
+    }
+    
+    func fetch<T: Decodable>(_ type: T.Type, from url: URL, completion: @escaping (Result<T, NetworkError>) -> Void) {
         URLSession.shared.dataTask(with: gamesURL) { data, _, error in
             guard let data else {
                 print(error?.localizedDescription ?? "No error description")
@@ -23,10 +41,12 @@ final class NetworkManager {
             
             do {
                 let decoder = JSONDecoder()
-                let games = try decoder.decode([Game].self, from: data)
-                print(games)
+                let dataModel = try decoder.decode(T.self, from: data)
+                DispatchQueue.main.async {
+                    completion(.success(dataModel))
+                }
             } catch {
-                print(error.localizedDescription)
+                completion(.failure(.decodingError))
             }
         }.resume()
     }
